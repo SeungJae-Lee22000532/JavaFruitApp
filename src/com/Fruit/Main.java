5d9fa7dc3e3b91f8e3d8952957ef306b1983545e
SeungJae-Lee22000532 <22000532@handong.edu>
2021-09-12 21:03:51 +0900

    Create and Modify Main.java and Fruit.java

---
+++ src/com/Fruit/Main.java
@@ -0,0 +1,37 @@
+package com.Fruit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	public static void main(String args[]) {
+		List<Fruit> fl = new ArrayList<Fruit>();
+		System.out.println("Fruit LIst (ordered by name)");
+		Fruit s1 = new Fruit(101, "Apple", 1000);
+		fl.add(s1);
+		Fruit s2 = new Fruit(102, "Orange", 1500);
+		fl.add(s2);
+		fl.add(new Fruit(103, "Kiwi", 2000));
+		fl.add(new Fruit(104, "Banana", 2500));
+		fl.add(new Fruit(105, "Melon", 3000));
+		fl.add(new Fruit(106, "Watermelon", 3500));
+		fl.add(new Fruit(107, "Mango", 4000));
+		fl.add(new Fruit(108, "Grape", 4500));
+		fl.add(new Fruit(109, "Peach", 5000));
+		fl.add(new Fruit(110, "Plum", 5500));
+		
+		Collections.sort(fl, new FruitComparator());
+		
+		for (Fruit s : fl) {
+			System.out.println(s.toString());
+		}
+		System.out.println("Fruit List (reverse ordered by name)");
+		
+		Collections.sort(fl, new FruitComparatorDesc());
+		
+		for (int i = 0; i < fl.size(); i++) {
+			System.out.println(fl.get(i).toString());
+		}
+	}
+}
